8aa5ad9120246cd216d487c5890709a664ecaa82
Khoj Badami <khoj.badami@gmail.com>
2017-01-18 08:10:18 +0530

    Made generator simply make copy of entire folder from the templates folder

--- lib/rush/generator.rb
+++ lib/rush/generator.rb
@@ -4,11 +4,12 @@ module Rush
 
     def initialize project_path
       @project_path = project_path
-      @path_to_templates_folder = File.expand_path( "../../templates", __FILE__ )
+      @path_to_templates_folder = File.expand_path( "../../templates/starter_app", __FILE__ )
     end
 
     def make_project
       delete_folder_at_path
+      FileUtils.copy_entry @path_to_templates_folder, @project_path
     end
 
     private
